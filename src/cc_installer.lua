26a3483d8f90578fecb7a470b4b9ef5e3783cb7b
Mason <mason.grosset@gmail.com>
2024-06-02 19:05:00 -0400

    Auto installation

--- src/cc_installer.lua
+++ src/cc_installer.lua
@@ -1,9 +1,20 @@
+
 local function install_github()
     shell.run("pastebin run p8PJVxC4")
 end
 
+local function download_self()
+    shell.run("github clone MnMaxon/cc_scripts -b main")
+end
+
 local function setup()
-    install_github()
+    if(pcall(install_github()))
+    then
+        print("Installed github")
+    else
+        print("Failed to install github - I hope you have it installed already!")
+    end
+    download_self()
 end
 
 setup()
