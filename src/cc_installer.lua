5b77adc75c2652cceb44bdebd4d8666594428436
Mason <mason.grosset@gmail.com>
2024-06-02 19:08:49 -0400

    Auto installation fixes

--- src/cc_installer.lua
+++ src/cc_installer.lua
@@ -8,11 +8,9 @@ local function download_self()
 end
 
 local function setup()
-    if pcall(install_github())
-    then
-        print("Installed github")
-    else
-        print("Failed to install github - I hope you have it installed already!")
+    local ok, err = pcall(install_github)
+    if ok then print("Installed github")
+    else print("Failed to install github - I hope you have it installed already!")
     end
     download_self()
 end
