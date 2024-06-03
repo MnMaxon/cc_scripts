189d036b5bd1e0b966d3c39e71f00fb48448266e
Mason <mason.grosset@gmail.com>
2024-06-02 20:37:14 -0400

    Added more components

--- src/basalt_test.lua
+++ src/basalt_test.lua
@@ -2,22 +2,61 @@ local basalt = require("basalt")
 
 local function test()
     local frame = basalt.createFrame()
-    local button = frame --> Basalt returns an instance of the object on most methods, to make use of "call-chaining"
-            :addButton() --> This is an example of call chaining
-            :setPosition(4, 4)
-            :setText("Click me!")
-            :onClick(
-            function()
-                basalt.debug("I got clicked!")
-            end)
-    local aDropdown = frame
+    local title_label = main:addLabel()
+            :setText("Spatial Manager")
+            :setFontSize(2)
+    local cat_label = main:addLabel()
+            :setText("Category: ")
+            :setFontSize(2)
+    local cat_dropdown = frame
             :addDropdown()
+            :setPosition(4, 2)
             :addItem("Item 1")
             :addItem("Item 2", colors.yellow)
             :addItem("Item 3", colors.yellow, colors.green)
             :onChange(
             function(self, item)
                 basalt.debug("Selected item: ", item.text)
+            end)
+    local unload_button = frame:addButton()
+            :setPosition(4, 4) -- TODO Move
+            :setText("Unload")
+            :onClick(
+            function()
+                -- TODO
+                basalt.debug("I got clicked!")
+            end)
+    local add_button = frame:addButton()
+            :setPosition(4, 4) -- TODO Move
+            :setText("Add")
+            :onClick(
+            function()
+                -- TODO
+                basalt.debug("I got clicked!")
+            end)
+    local remove_button = frame:addButton()
+            :setPosition(4, 4) -- TODO Move
+            :setText("Remove")
+            :onClick(
+            function()
+                -- TODO
+                basalt.debug("I got clicked!")
+            end)
+    local up_button = frame:addButton()
+            :setPosition(4, 4) -- TODO Move
+            :setText("^ UP")
+            :onClick(
+            function()
+                -- TODO
+                basalt.debug("I got clicked!")
+            end)
+    local down_button = frame:addButton()
+            :setPosition(4, 4) -- TODO Move
+            :setText("v DOWN")
+            :onClick(
+            function()
+                -- TODO
+                basalt.debug("I got clicked!")
             end)
     basalt.autoUpdate()
 end
