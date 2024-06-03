1c522199fbf3259e061fe32fe6403d3d6a349f9f
Mason <mason.grosset@gmail.com>
2024-06-03 02:42:35 -0400

    spatial work

--- src/spatial.lua
+++ src/spatial.lua
@@ -12,16 +12,15 @@ local storage_name = "top"
 local function update_dropdown(dropdown, vals)
     local val = dropdown.getValue()
     dropdown:clear()
-    for i, cat in ipairs(vals) do
+    if vals == nil then
+        return
+    end
+    for i, cat in pairs(vals) do
         dropdown:addItem(cat)
         if val ~= nil and cat == val.text then
             dropdown:selectItem(i)
         end
     end
-end
-
-local function get_storage()
-    return peripheral.wrap(storage_name)
 end
 
 local function find_peripheral(name)
@@ -34,22 +33,12 @@ local function find_peripheral(name)
     return nil
 end
 
-local function get_io_name()
-    local names = peripheral.getNames()
-    for _, name in ipairs(names) do
-        if string.find(name, "ae2:spatial_io_port") then
-            return name
-        end
-    end
-    return nil
+local function get_io_port()
+    return find_peripheral("ae2:spatial_io_port")
 end
 
-local function get_io_port()
-    local name = get_io_name()
-    if name == nil then
-        return nil
-    end
-    return peripheral.wrap(name)
+local function get_storage()
+    return find_peripheral("minecraft:barrel")
 end
 
 local function remove_active()
